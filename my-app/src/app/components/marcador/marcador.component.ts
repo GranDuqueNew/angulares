360209a26eca0ee44c2e40e40c9d3aa4fbcee29f
valexx55 <valexx55@gmail.com>
2022-02-10 19:55:19 +0100

    solucion guardar borrar marcador

--- my-app/src/app/components/marcador/marcador.component.ts
+++ my-app/src/app/components/marcador/marcador.component.ts
@@ -8,6 +8,13 @@ import { Marcador } from 'src/app/models/marcador';
 })
 export class MarcadorComponent implements OnInit {
 
+//TODO: anañadid dos botones al pie del marcador
+//con las opciones de Guardar y Borrar
+//GUARDAR
+//la primera, persiste el marcador en el localStorage
+//de tal manera, que cuanado entre ese marcador guardado se muestre
+//BORRAR
+//y la opción de borrar, que limpie el marcador almacenado
 
   marcador_actual:Marcador;
   resultado:string;
@@ -20,6 +27,24 @@ export class MarcadorComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("ngOnInit MarcadorComponent");
+    this.marcador_actual = this.obtenerMarcadorGuardado ();
+  }
+
+  obtenerMarcadorGuardado ():Marcador
+  {
+    let marcador_aux:Marcador=new Marcador();
+    let string_marcador_guardado:string|null;
+
+      string_marcador_guardado = localStorage.getItem('puntuaciones');
+      if (string_marcador_guardado)
+      {
+        //hay un marcador guardado
+        //marcador_aux = <Marcador>JSON.parse(string_marcador_guardado);//Casting
+        marcador_aux = JSON.parse(string_marcador_guardado) as Marcador;
+      }
+
+
+    return marcador_aux;
   }
 
   actualizarMarcador (resultado:number):void
@@ -43,6 +68,21 @@ export class MarcadorComponent implements OnInit {
           this.marcador_actual.puntuacion_jugador = this.marcador_actual.puntuacion_jugador+1;
           break;
    }
+  }
+
+  guardarMarcador()
+  {
+    console.log("gurdar marcador");
+    let marcador_actual_json:string = JSON.stringify(this.marcador_actual);
+    localStorage.setItem('puntuaciones', marcador_actual_json);
+  }
+  borrarMarcador()
+  {
+    console.log("borrar marcador");
+    //this.marcador_actual=new Marcador();
+    this.marcador_actual.puntuacion_jugador=0;
+    this.marcador_actual.puntuacion_maquina=0;
+    localStorage.removeItem('puntuaciones');
   }
 
 }
